fc750e69a0a720e37b20281d6c840ea0fc4bdab4
sulsyh <71731748+sulsyh@users.noreply.github.com>
2020-10-16 23:54:29 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,13 @@
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pkg install figlet
+pkg install Ruby
+pkg install lolcat
+gem install lolcat
+pkg install curl
+pip install termcolor
+clear
+figlet -f slant Suscses | lolcat
+echo "Selanjutnya Ketik sh SulSyh.sh"
+
